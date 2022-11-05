f8eb02f884166e9d2eebfe254ed2953f2cecc244
TirteaGabriel <gabrieltirtea@gmail.com>
2022-11-05 16:54:48 +0200

    p4

--- Probleme - setul 2/Program.cs
+++ Probleme - setul 2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,56 @@ namespace Probleme___setul_2
         }
         static void p2()
         {
+            int nrpoz = 0;
+            int nrneg = 0;
+            int nrzero = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x < 0)
+                    nrneg++;
+                if (x > 0)
+                    nrpoz++;
+                if (x == 0)
+                    nrzero++;
+            }
+            Console.WriteLine("Sunt " + nrpoz + " numere pozitive in secventa data");
+            Console.WriteLine("Sunt " + nrneg + " numere negative in secventa data");
+            Console.WriteLine("Sunt " + nrzero + " numere zero in secventa data");
+        }
+        static void p3()
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+            ulong p = 1;
+            ulong s = 0;
+            for (ulong i = 1; i <= n; i++)
+            {
+                s += i;
+                p *= i;
+            }
+            Console.WriteLine("Suma numerelor de la 1 pana la " + n + " este " + s);
+            Console.WriteLine("Produsul numerelor de la 1 pana la " + n + " este " + p);
+        }
+        static void p4()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int poz = -1;
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if (x == a)
+                    poz = i;
+            }
+            if (poz == -1)
+                Console.WriteLine(poz);
+            else
+                Console.WriteLine("Pozitia lui " + a + " este " + poz);
+        }
+        static void  p5()
+        {
+            int n = int.Parse(Console.ReadLine());
 
         }
         static void Main(string[] args)
@@ -37,17 +88,17 @@ namespace Probleme___setul_2
                 if (nrp == 2)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p2();
+                    p2();
                 }
                 if (nrp == 3)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p3();
+                    p3();
                 }
                 if (nrp == 4)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p4();
+                    p4();
                 }
                 if (nrp == 5)
                 {
